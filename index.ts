854c6061093ab054083e8e15bff75f4eb4767ffb
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-31 15:50:22 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+import * as Http from "effect-http";
+
+import { pipe } from "@effect/data/Function";
+import * as Effect from "@effect/io/Effect";
+import * as Schema from "@effect/schema/Schema";
+
+const responseSchema = Schema.struct({ name: Schema.string });
+const query = { id: Schema.number };
+
+const api = pipe(
+  Http.api({ title: "Users API" }),
+  Http.get("getUser", "/user", {
+    response: responseSchema,
+    query: query,
+  }),
+);
+
+const server = pipe(
+  api,
+  Http.server,
+  Http.handle("getUser", ({ query }) => Effect.succeed({ name: "milan" })),
+  Http.exhaustive,
+);
+
+const client = pipe(api, Http.client(new URL("http://localhost:3000")));
+
+const callServer = () =>
+  pipe(
+    client.getUser({ query: { id: 12 } }),
+    Effect.flatMap((user) => Effect.logInfo(`Got ${user.name}, nice!`)),
+  );
+
+pipe(
+  server,
+  Http.listen({ port: 3000, onStart: callServer }),
+  Effect.runPromise,
+);
